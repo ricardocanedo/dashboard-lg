a8c6339a1f91172de0edfae04741e59494baed2f
Ricardo Canedo <ricardocanedo@live.com>
2026-02-05 18:28:01 -0300

    fix: namespace and fillable

--- app/Model/ProductionLine.php
+++ app/Model/ProductionLine.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -10,7 +10,7 @@ class ProductionLine extends Model
     use SoftDeletes;
 
     protected $fillable = [
-        'name', 
+        'name',
         'plant_id'
     ];
 
--- app/Model/ProductionRecord.php
+++ app/Model/ProductionRecord.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -11,7 +11,7 @@ class ProductionRecord extends Model
 
     protected $fillable = [
         'production_line_id',
-        'date',
+        'production_date',
         'good_parts',
         'defective_parts',
         'efficiency',
