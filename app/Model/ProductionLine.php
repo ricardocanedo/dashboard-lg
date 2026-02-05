e5c8dcc1d5fe03c422fa21c9b3a83109b2d41b91
Ricardo Canedo <ricardocanedo@live.com>
2026-02-05 15:49:51 -0300

    models fillables

--- app/Model/ProductionLine.php
+++ app/Model/ProductionLine.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductionLine extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name', 
+        'plant_id'
+    ];
 }
--- app/Model/ProductionRecord.php
+++ app/Model/ProductionRecord.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ProductionRecord extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $fillable = [
+        'production_line_id',
+        'date',
+        'good_parts',
+        'defective_parts',
+        'efficiency',
+    ];
 }
